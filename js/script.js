f30658ba7e5ec4b86f5203a25b269156965ff2e5
Decline-of-Mind <L.h.gotink@gmail.com>
2020-01-09 16:44:37 +0000

    added Jquery for panels

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
- // put your code here
+    $(".theButton").on("click", function() {
+        $(".container").siblings().fadeTo("slow", 0.1);
+    });
+     $(".superButton").click(function (){
+        $(".container").siblings().fadeTo("slow", 1);
+    });
+    $(".theButton").mouseenter(function() {
+        $(this).addClass(".turnBlack");
+    });
+    $(".theButton").mouseleave(function() {
+        $(this).removeClass(".turnBlack");
+    });
 });
