54df784ac633ab562410f7128692aa1c5ee5e69b
Decline-of-Mind <L.h.gotink@gmail.com>
2020-01-09 17:20:14 +0000

    finished other traversing methods

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ $(document).ready(function(){
         $(".container").siblings().fadeTo("slow", 1);
     });
     $(".theButton").mouseenter(function() {
-        $(this).addClass(".turnBlack");
+        $(this).addClass("makeBlack");
     });
     $(".theButton").mouseleave(function() {
-        $(this).removeClass(".turnBlack");
+        $(this).removeClass("makeBlack");
+    });
+    $(".theButton").click( function() {
+        var panelColor = $(this).css("background-color");
+        $("superButton").text(panelColor);
+    });
+});
+//--- for Challenge_B.html ---//
+$(document).ready(function(){
+    $("th").on("click", function() {
+        $("tr").children().removeClass("selection");
+        $(this).siblings().addClass("selection");
     });
 });
